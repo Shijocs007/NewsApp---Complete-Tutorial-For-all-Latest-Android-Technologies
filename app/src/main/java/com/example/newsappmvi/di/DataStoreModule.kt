16a24cb1f86b7334f4adde3afdf1ddd1d55b1ac0
shijo <shijo.cs@awarex.com>
2024-08-14 15:08:27 +0530

    dependencies and onboarding screen added

---
+++ app/src/main/java/com/example/newsappmvi/di/DataStoreModule.kt
@@ -0,0 +1,27 @@
+package com.example.newsappmvi.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    private const val DATASTORE_NAME = "news_app_datastore_preferences"
+
+    private val Context.dataStore by preferencesDataStore(name = DATASTORE_NAME)
+
+    @Singleton
+    @Provides
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.dataStore
+    }
+}
